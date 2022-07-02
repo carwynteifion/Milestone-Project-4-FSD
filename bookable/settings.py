ba9cb85c890b481a3f589f7cb4be7a7f329517d2
carwynteifion <carwynteifion@yahoo.co.uk>
2022-07-02 21:36:17 +0000

    Update secret key and debug settings

--- bookable/settings.py
+++ bookable/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-va4lg5qqvbof=e(=$=l6j7&6d^lux(@#nzq+8op1-gwdqa(5ti"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['milestone-project-4-fsd.herokuapp.com', 'localhost']
 
