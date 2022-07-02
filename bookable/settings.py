edb7920917c8c09442e4fde49a60454d9524ad59
carwynteifion <carwynteifion@yahoo.co.uk>
2022-07-02 20:33:34 +0000

    Deploy to Heroku

--- bookable/settings.py
+++ bookable/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-va4lg5qqvbof=e(=$=l6j7&6d^lux(@#nzq+8op1-gwdqa(5ti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['milestone-project-4-fsd.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -107,7 +107,7 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 ACCOUNT_AUTHENTICATION_METHOD = "username_email"
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION = "none"
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = "/accounts/login/"
@@ -120,12 +120,15 @@ WSGI_APPLICATION = "bookable.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if 'DATABASE_URL' in os.environ:
+    dj_database_url.parse(os.environ.get('DATABASE_URL'))
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
