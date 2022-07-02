3ab42a75acf99b738ae4fb2273feeadbad19cd86
carwynteifion <carwynteifion@yahoo.co.uk>
2022-07-02 18:39:38 +0000

    Migrate databases to postgres

--- bookable/settings.py
+++ bookable/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,11 +120,16 @@ WSGI_APPLICATION = "bookable.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse("postgres://gemlnnbmszqcpm:d4caed7791678dd345bbe26537545a540fc2e7c5e7d7a091fa17c6fb0d9fbdcd@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/de8j864gr6jmeq")
+
 }
 
 
