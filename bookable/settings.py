9e93d55ba77b10649b38724aa3cc533efd7826a5
carwynteifion <carwynteifion@yahoo.co.uk>
2022-07-03 00:40:18 +0000

    Update settings

--- bookable/settings.py
+++ bookable/settings.py
@@ -95,12 +95,12 @@ TEMPLATES = [
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
-AUTHENTICATION_BACKENDS = [
+AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
     "django.contrib.auth.backends.ModelBackend",
     # `allauth` specific authentication methods, such as login by e-mail
     "allauth.account.auth_backends.AuthenticationBackend",
-]
+)
 
 SITE_ID = 1
 
@@ -122,7 +122,9 @@ WSGI_APPLICATION = "bookable.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    DATABASES = {	
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 else:
     DATABASES = {
         "default": {
